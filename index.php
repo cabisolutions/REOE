9107b4c8cd452c13cf47fbee9cb7eb60ae33a41b
2021 DSM31 Angel Flores Carlos <angelfc.contact@gmail.com>
2021-07-09 13:02:59 -0500

    Bosquejo de Index.php

--- index.php
+++ index.php
@@ -6,13 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Renta de espacios | REOE</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!--link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
+    crossorigin="anonymous"-->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/estilosGlobales.css">
 </head>
 
 <body>
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-nav">   
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-nav">
         <div class="container">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -36,15 +39,28 @@
         </div>
     </nav>
     <div id="carrsel" class="carousel slide" data-bs-ride="carousel">
+
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <img src="img/espacio1.jpg" class="d-block w-100 indexCarousel" alt="Espacio para eventos con pista de baile">
+                <div class="carousel-caption">
+                    <h1 class="display-2 text-white mt-5 mb-3">Espacios para eventos y oficinas</h1>
+                    <button class="btn btn-secondary mb-5">Renta ahora</button>
+                </div>
             </div>
             <div class="carousel-item">
                 <img src="img/oficina1.jpg" class="d-block w-100 indexCarousel" alt="...">
+                <div class="carousel-caption">
+                    <h1 class="display-2 text-white mt-5 mb-3">Revisa nuestro catálogo virtual</h1>
+                    <button class="btn btn-secondary mb-5">Ir a catálogo</button>
+                </div>
             </div>
             <div class="carousel-item">
                 <img src="img/oficina2.jpg" class="d-block w-100 indexCarousel" alt="...">
+                <div class="carousel-caption">
+                    <h1 class="display-2 text-white mt-5 mb-3">¿Más información o dudas?</h1>
+                    <button class="btn btn-secondary mb-5">Contáctanos</button>
+                </div>
             </div>
         </div>
     </div>
