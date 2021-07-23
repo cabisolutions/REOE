9a2f8bc1b0bd59293d5f9333e1f2d4292dde4c00
2021 DSM31 Angel Flores Carlos <angelfc.contact@gmail.com>
2021-07-23 15:40:21 -0500

    ➕ADD: Enrutamiento de url más amigables Uso de la libreria steampixel/simple-php-router

---
+++ inicio.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Renta de espacios | REOE</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/estilosGlobales.css">
+</head>
+
+<body>
+    <?php
+    include('./menu.php')
+    ?>
+    <div id="carousel-index" class="carousel slide carousel-fade" data-bs-ride="carousel">
+        
+        <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="7000">
+                <img src="img/espacio1.jpg" class="d-block w-100 indexCarousel" alt="Espacio para eventos con pista de baile">
+                <div class="carousel-caption">
+                    <h1 class="display-3 text-white mb-5">Encuentra los mejores espacios</h1>
+                </div>
+            </div>
+            <div class="carousel-item" data-bs-interval="7000">
+                <img src="img/oficina1.jpg" class="d-block w-100 indexCarousel" alt="Espacio para eventos con pista de baile">
+                <div class="carousel-caption">
+                    <h1 class="display-3 text-white mb-5">Espacios para eventos y oficinas</h1>
+                </div>
+            </div>
+            <div class="carousel-item" data-bs-interval="7000">
+                <img src="img/oficina2.jpg" class="d-block w-100 indexCarousel" alt="Espacio para eventos con pista de baile">
+                <div class="carousel-caption">
+                    <h1 class="display-3 text-white mb-5">Revisa nuestro catálogo virtual</h1>
+                </div>
+            </div>
+        </div>
+        <form action="catalogo" method="GET" class="buscador">
+            <input class="form-control" name="busqueda" id="busqueda" type="search" placeholder="¿Qué estás buscando?">
+            <button class="btn btn-success" type="submit">Buscar</button>
+        </form>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carousel-index" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carousel-index" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
--- menu.php
+++ menu.php
@@ -10,16 +10,19 @@
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Inicio</a>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="catalogo" id="navbarScrollingDropdown" role="button"  aria-expanded="false">
                             Catálogo
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-                            <li><a class="dropdown-item" href="#">Eventos</a></li>
+                            <li><a class="dropdown-item" href="catalogo">Ver todo</a></li>
+                            <li><a class="dropdown-item" href="catalogo">Eventos</a></li>
                             <li><a class="dropdown-item" href="#">Oficinas</a></li>
                         </ul>
                     </li>
                 </ul>
             </div>
             <a href="./sesion.php" class="btn btn-secondary btn-sm">Iniciar sesión</a>
+            <script src="./js/jquery-3.6.0.min.js"></script>
+            <script src="./js/menu.js"></script>
         </div>
     </nav>
