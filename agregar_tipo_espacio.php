09771507ae335dba6ca692dfe85123dbaac81f20
franckkgp <franckkgp@gmail.com>
2021-08-04 00:47:48 -0500

    detalle_espacio.php

--- agregar_tipo_espacio.php
+++ agregar_tipo_espacio.php
@@ -27,7 +27,12 @@ if ('GET' == $_SERVER['REQUEST_METHOD'] && isset($_GET['id']) && is_numeric($_GE
 </head>
 <body>
 <?php
-?>
+    include_once './menu.php';
+    ?>
+    <div class="d-flex mt-5">
+        <?php
+        include_once('menu_admin.php');
+        ?>
 <div class="container mt-3">
     <div class="row">
         <div class="col-3"></div>
